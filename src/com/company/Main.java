7957e836ed2ce192138a16b87bd076b385b7cd06
zxx <71894437@qq.com>
2022-06-14 22:01:32 +0800

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git3");
+
     }
 }
